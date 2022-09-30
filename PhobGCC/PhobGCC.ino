4b49ff46dd95e04832fb5cb9d8e63a645a1ea3f7
Savestate2a03 <joseph.elkhouri+github@gmail.com>
2022-09-30 19:25:04 +0200

    Add toggleable ESS adapter

--- PhobGCC/PhobGCC.ino
+++ PhobGCC/PhobGCC.ino
@@ -27,6 +27,7 @@ extern "C" uint32_t set_arm_clock(uint32_t frequency);
 #define SW_VERSION 25
 
 //#define ENABLE_LED
+#define ENABLE_ESS
 
 using namespace Eigen;
 
@@ -90,6 +91,10 @@ enum JumpConfig {
 	SWAP_YZ
 };
 
+enum EssConfig {
+	ESS_DISABLED,
+	ESS_ENABLED
+};
 
 //defining control configuration
 int _pinZSwappable = _pinZ;
@@ -125,6 +130,9 @@ const int _rumbleMax = 7;
 const int _rumbleDefault = 5;
 bool _safeMode = true;
 bool _autoInit = false;
+
+//ESS Adapter settings
+EssConfig _essEnable = ESS_DISABLED;
 
 int trigL,trigR;
 
@@ -282,6 +290,7 @@ const int _eepromCxSmoothing = _eepromROffset+_bytesPerFloat;
 const int _eepromCySmoothing = _eepromCxSmoothing+_bytesPerFloat;
 const int _eepromRumble = _eepromCySmoothing+_bytesPerFloat;
 const int _eepromAutoInit = _eepromRumble+_bytesPerFloat;
+const int _eepromEssEnable = _eepromAutoInit+_bytesPerFloat;
 
 Bounce bounceDr = Bounce();
 Bounce bounceDu = Bounce();
@@ -303,6 +312,10 @@ float _xPosList[MEDIANLEN];//for median filtering
 float _yPosList[MEDIANLEN];//for median filtering
 unsigned int _xMedianIndex;
 unsigned int _yMedianIndex;
+
+//origin variables
+unsigned int _intOrigin = 127;
+float _floatOrigin = 127.5;
 
 //new kalman filter state variables
 float _xPos;//input of kalman filter
@@ -420,12 +433,110 @@ volatile char _commResponse[_originLength] = {
     0,0,0,0,0,0,0,0};
 #endif // TEENSY4_0
 
+namespace ess {
+	/* This namespace uses MIT Licensed code from Skuzee's ESS Adapter 
+	 * code to reverse WiiVC mapping in Ocarina of Time. Please check out      
+	 * the source for that here: https://github.com/Skuzee/ESS-Adapter 
+	 */
+
+	// ALL COMMENTS REGARDING HOW THIS WORKS HAVE BEEN REMOVED.
+	// Please refer to the original code for notes. This code is
+	// likely never changing unless we implement a float interpretation.
+
+	const char one_dimensional_map[] = "\x00\x00\x10\x10\x11\x11\x12\x12\x13\x13\x14\x14\x15\x15\x16\x16\x16\x17\x17\x17\x18\x18\x19\x19\x1a\x1a\x1a\x1b\x1b\x1b\x1c\x1c\x1d\x1d\x1d\x1e\x1e\x1e\x1f\x1f  !!!\"\"\"###$$$%%%&&&'''((()))***+++,,,,---...///00001111222333344445555666677778888899999::::;;;;;<<<<<=====>>>>>??????@@@";
+	const char triangular_map[] = ",,-,.,.,/,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,9,:,:,;,;,<,<,<,=,=,>,>,>,?,?,?,@,--.-.-/-0-0-1-1-2-2-3-3-4-4-5-5-6-6-7-7-8-8-9-9-9-:-:-;-;-<-<-<-=-=->->->-?-?-?-@,..../.0.0.1.1.2.2.3.3.4.4.5.5.6.6.7.7.8.8.9.9.9.:.:.;.;.<.<.<.=.=.>.>.>.?-?-?-?-../.0.0.1.1.2.2.3.3.4.4.5.5.6.6.7.7.8.8.9.9.9.:.:.;.;.<.<.<.=.=.>.>.>.?-?-?-?-//0/0/1/1/2/2/3/3/4/4/5/5/6/6/7/7/8/8/9/9/9/:/:/;/;/</</</=/=/>/>/>/>/>/?-?-000010102020303040405050606070708080909090:0:0;0;0<0<0<0=0=0>/>/>/>/>/>/>/0010102020303040405050606070708080909090:0:0;0;0<0<0<0=0=0=0>/>/>/>/>/>/11112121313141415151616171718181919191:1:1;1;1<1<1<1=0=0=0>/>/>/>/>/>/112121313141415151616171718181919191:1:1;1;1<1<1<1<1<1=0=0>/>/>/>/>/2222323242425252626272728282929292:2:2;2;2<1<1<1<1<1<1=0=0>/>/>/>/22323242425252626272728282929292:2:2;2;2;2<1<1<1<1<1<1<1=0=0>/>/333343435353636373738383939393:3:3;3;3;3;3<1<1<1<1<1<1<1=0=0>/3343435353636373738383939393:3:3;3;3;3;3;3<1<1<1<1<1<1<1<1=044445454646474748484949494:4:4:4;3;3;3;3;3<1<1<1<1<1<1<1<1445454646474748484949494:4:4:4:4;3;3;3;3;3;3<1<1<1<1<1<1555565657575858595959595:4:4:4:4;3;3;3;3;3;3<1<1<1<1<1556565757585859595959595:4:4:4:4;3;3;3;3;3;3<1<1<1<1666676768686869595959595:4:4:4:4;3;3;3;3;3;3;3<1<1667676868686959595959595:4:4:4:4:4;3;3;3;3;3;3<1777777868686959595959595:4:4:4:4:4;3;3;3;3;3;3777777868686869595959595:4:4:4:4:4;3;3;3;3;377777786868686959595959595:4:4:4:4:4;3;3;377777786868686959595959595:4:4:4:4:4;3;377777786868686959595959595:4:4:4:4:4;377777786868686959595959595:4:4:4:4:477777786868686959595959595:4:4:4:47777778686868695959595959595:4:47777778686868695959595959595:4777777868686869595959595959577777786868686959595959595777777868686869595959595777777868686869595959577777786868686869595777777868686868695777777868686868677777786868686777777868686777777868677777786777777777777";
+
+	const uint32_t OOT_MAX = 80;
+	const uint32_t BOUNDARY = 39;
+
+	void gc_to_n64(uint8_t coords[2]) {
+	  uint32_t scale = 5L * coords[0] + 2L * coords[1];
+	  if (scale > 525) {
+	    scale = 16UL * 525 * 525 * 525 / scale;
+	  } else {
+	    scale = 16 * scale * scale;
+	  }
+	  scale *= coords[1];
+	  scale = scale * 2 / 115;
+	  scale += 25565300; 
+	  coords[0] = (coords[0] * scale + 16774000) >> 24;
+	  coords[1] = (coords[1] * scale + 16774000) >> 24;
+	}
+
+	uint16_t triangular_to_linear_index(uint8_t row, uint8_t col, uint8_t size) {
+	  return (size * (size - 1) / 2) - (size - row) * ((size - row) - 1) / 2 + col;
+	}
+
+	void invert_vc(uint8_t coords[2]) {
+	  if (coords[0] > 2 * OOT_MAX) coords[0] = 2 * OOT_MAX;
+	  if (coords[1] > 2 * OOT_MAX) coords[1] = 2 * OOT_MAX;
+	  if (coords[0] >= 2 * BOUNDARY && coords[1] >= 2 * BOUNDARY) {
+	    uint8_t remainder = OOT_MAX + 1 - BOUNDARY;
+	    coords[0] = (coords[0] / 2) - BOUNDARY;
+	    coords[1] = (coords[1] / 2) - BOUNDARY;
+	    uint16_t index = triangular_to_linear_index(coords[1], coords[0], remainder);
+	    coords[0] = pgm_read_byte(triangular_map + 2 * index);
+	    coords[1] = pgm_read_byte(triangular_map + 2 * index + 1);
+	  } else {
+	    coords[0] = pgm_read_byte(one_dimensional_map + coords[0]);
+	    coords[1] = pgm_read_byte(one_dimensional_map + coords[1]);
+	  }
+	}
+
+	void invert_vc_gc(uint8_t coords[2]) {
+	  int x_positive = 0;
+	  int y_positive = 0;
+	  int swap = 0;
+
+	  if (coords[0] >= 128) {
+	    x_positive = 1;
+	    coords[0] -= 128;
+	  } else {
+	    if (coords[0] == 0) coords[0] = 127;
+	    else coords[0] = 128 - coords[0];
+	  }
+
+	  if (coords[1] >= 128) {
+	    y_positive = 1;
+	    coords[1] -= 128;
+	  } else {
+	    if (coords[1] == 0) coords[1] = 127;
+	    else coords[1] = 128 - coords[1];
+	  }
+
+	  if (coords[1] > coords[0]) {
+	    swap = 1;
+	    uint8_t temp = coords[0];
+	    coords[0] = coords[1];
+	    coords[1] = temp;
+	  }
+
+	  gc_to_n64(coords);
+	  invert_vc(coords);
+
+	  if (swap) {
+	    uint8_t temp = coords[0];
+	    coords[0] = coords[1];
+	    coords[1] = temp;
+	  }
+
+	  if (x_positive) coords[0] += 128;
+	  else coords[0] = 128 - coords[0];
+	  if (y_positive) coords[1] += 128;
+	  else coords[1] = 128 - coords[1];
+	}
+}
+
 void setup() {
     serialSetup();
 	Serial.print("Software version 0.");
 	Serial.println(SW_VERSION);
 #ifdef BUILD_DEV
 	Serial.println("This is not a stable version");
+#endif
+
+#ifdef ENABLE_ESS
+	Serial.println("ESS functionality enabled");
 #endif
 
 #ifdef TEENSY4_0
@@ -1191,6 +1302,12 @@ int readEEPROM(){
 	notchCalibrate(_cleanedPointsX, _cleanedPointsY, _notchPointsX, _notchPointsY, _noOfNotches, _cAffineCoeffs, _cBoundaryAngles);
 	//stickCal(_cleanedPointsX,_cleanedPointsY,_cNotchAngles,_cFitCoeffsX,_cFitCoeffsY,_cAffineCoeffs,_cBoundaryAngles);
 
+	//get the settings for ess functionality
+	EEPROM.get(_eepromEssEnable, _essEnable);
+#ifndef ENABLE_ESS
+	_essEnable = ESS_DISABLED;
+#endif
+
 	return numberOfNaN;
 }
 
@@ -1238,6 +1355,9 @@ void resetDefaults(bool resetSticks){
 	_rumble = _rumbleDefault;
 	_rumblePower = calcRumblePower(_rumble);
 	EEPROM.put(_eepromRumble, _rumble);
+
+	_essEnable = ESS_DISABLED;
+	EEPROM.put(_eepromEssEnable, _essEnable);
 
 	//always cancel auto init on reset, even if we don't reset the sticks
 	_autoInit = 0;
@@ -1546,6 +1666,10 @@ void readButtons(){
 			adjustTriggerOffset(true, false, true);
 		} else if(hardwareR && hardwareZ && btn.Dd) { //Decrease R-trigger Offset
 			adjustTriggerOffset(true, false, false);
+#ifdef ENABLE_ESS
+		} else if(btn.Ay < (_intOrigin-48) && btn.Cy < (_intOrigin-48) && btn.A && btn.Dd) { //Toggle ESS
+			toggleEss();
+#endif
 		} else if(hardwareX && hardwareZ && btn.S) { //Swap X and Z
 			readJumpConfig(SWAP_XZ);
 			freezeSticks(2000);
@@ -2200,6 +2324,31 @@ void nextTriggerState(int _currentConfig, bool _lTrigger) {
 
 	clearButtons(2000);
 }
+void toggleEss() {
+	if (_essEnable) {
+		Serial.println("Disabling ESS");
+		_essEnable = ESS_DISABLED;
+	} else {
+		Serial.println("Enabling ESS");
+		_essEnable = ESS_ENABLED;
+	}
+
+	EEPROM.put(_eepromEssEnable, _essEnable);
+
+	if (_essEnable) {
+		btn.Ay = (uint8_t) (127.5 + 50);
+		btn.Ax = (uint8_t) (127.5 + 50);
+		btn.Cy = (uint8_t) (127.5 + 50);
+		btn.Cx = (uint8_t) (127.5 + 50);
+	} else {
+		btn.Ay = (uint8_t) (127.5 - 50);
+		btn.Ax = (uint8_t) (127.5 - 50);
+		btn.Cy = (uint8_t) (127.5 - 50);
+		btn.Cx = (uint8_t) (127.5 - 50);
+	}
+
+	clearButtons(2000);
+}
 void initializeButtons(Buttons &thisbtn,int &startUpLa, int &startUpRa){
 	//set the analog stick values to the chosen center value that will be reported to the console on startup
 	thisbtn.Ax = _analogCenter;
@@ -2375,16 +2524,30 @@ void readSticks(int readA, int readC){
 	remappedCx = min(125, max(-125, remappedCx+_cXOffset));
 	remappedCy = min(125, max(-125, remappedCy+_cYOffset));
 
+	//Apply ESS transformation
+	uint8_t remappedESS[2] = {
+		(uint8_t) (remappedAx + 128),
+		(uint8_t) (remappedAy + 128)
+	};
+	ess::invert_vc_gc(remappedESS);
+
 	float hystVal = 0.3;
 	//assign the remapped values to the button struct
 	if(readA){
-		float diffAx = (remappedAx+127.5)-btn.Ax;
-		if( (diffAx > (1.0 + hystVal)) || (diffAx < -hystVal) ){
-			btn.Ax = (uint8_t) (remappedAx+127.5);
+	if (_essEnable == ESS_ENABLED) {
+			// use ESS mapping for WiiVC if enabled
+			btn.Ax = remappedESS[0];
+			btn.Ay = remappedESS[1];
 		}
-		float diffAy = (remappedAy+127.5)-btn.Ay;
-		if( (diffAy > (1.0 + hystVal)) || (diffAy < -hystVal) ){
-			btn.Ay = (uint8_t) (remappedAy+127.5);
+		else {
+			float diffAx = (remappedAx+127.5)-btn.Ax;
+			if( (diffAx > (1.0 + hystVal)) || (diffAx < -hystVal) ){
+				btn.Ax = (uint8_t) (remappedAx+127.5);
+			}
+			float diffAy = (remappedAy+127.5)-btn.Ay;
+			if( (diffAy > (1.0 + hystVal)) || (diffAy < -hystVal) ){
+				btn.Ay = (uint8_t) (remappedAy+127.5);
+			}
 		}
 	}
 	if(readC){
